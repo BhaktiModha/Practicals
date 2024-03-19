9fdad35703bca413f62bd55c39560b28fd81036b
BhaktiModha <161273786+BhaktiModha@users.noreply.github.com>
2024-03-19 15:54:49 +0530

    Practical

---
+++ square and cube code.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+     long n,square,cube;
+
+     cout << "Enter the number :--> " ;
+     cin >> n ;
+
+     square = n * n;
+     cube = n * n * n;
+
+     cout << "Square of the number :--> " << square ;
+     cout << "\nCube of the number :--> " << cube ;
+
+    return 0;
+}
